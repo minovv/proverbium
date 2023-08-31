aeb2f2c79bc347753a2bb5642cec6d250c298662
Minov Vitaliy <minov.vitaliy@tirolhelpsukraine.eu>
2023-08-31 11:58:04 +0200

    Wer nicht hören will, muss fühlen.

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 function getProverb(sentence, callback) {
-  callback("Today's proverb is: " + sentence)
+  callback("The proverb of the day is: " + sentence)
 }
 
 getProverb("Wer dich vemisst, wird anrufen.", function (proverb) {
   console.log(proverb)
 })
+
+getProverb("Wer nicht hören will, muss fühlen", (proverb) =>
+  console.log(proverb)
+)
+
+let proverbOfTheDay = (a, b) => {
+  return a + b
+}
+
+console.log(
+  proverbOfTheDay(
+    "The proverb of the day is: ",
+    "Wer nicht hören will, muss fühlen."
+  )
+)
+
+let printName = (name) => {
+  console.log(name)
+}
+
+printName("Guest")
+
+let printHi = (name) => "Hi " + name + "!"
+
+console.log(printHi("Guest"))
