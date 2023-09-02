645c14b4f3a74728f1e6aba5c957f5ca743fb509
Minov Vitaliy <minov.vitaliy@tirolhelpsukraine.eu>
2023-09-02 12:24:02 +0200

    Reibung erzeugt Wärme.

--- script.js
+++ script.js
@@ -30,3 +30,44 @@ printName("Guest")
 let printHi = (name) => "Hi " + name + "!"
 
 console.log(printHi("Guest"))
+
+let proverbArray = [
+  {
+    id: 1,
+    sentence: "Es ist noch kein Meister vom Himmel gefallen.",
+    points: 1,
+  },
+  {
+    id: 2,
+    sentence:
+      "Wer dich vermisst, wird anrufen. Wer dich will, sagt es. Wer Interesse hat, zeigt es.",
+    points: 1,
+  },
+  {
+    id: 3,
+    sentence:
+      "Die Lüge hat zwar kurze Beine, rennt aber schneller als die Wahrheit.",
+    points: 1,
+  },
+  {
+    id: 4,
+    sentence: "Für Geld und gute Worte kann man alles haben.",
+    points: 1,
+  },
+  {
+    id: 5,
+    sentence: "Wer nicht hören will, muss fühlen.",
+    points: 1,
+  },
+  {
+    id: 6,
+    sentence: "Reibung erzeugt Wärme.",
+    points: 1,
+  },
+]
+
+let proverbCount = proverbArray.reduce((sum, item) => {
+  return sum + item.points
+}, 0)
+
+console.log("Total proverbs count: " + proverbCount)
