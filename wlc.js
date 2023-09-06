399ead16159db4d5904a3b2da7713116c7ad7e5a
Minov Vitaliy <minov.vitaliy@tirolhelpsukraine.eu>
2023-09-06 20:09:45 +0200

    Rom ist nicht an einem Tag erbaut worden.

---
+++ wlc.js
@@ -0,0 +1,7 @@
+const isLoggedIn = true
+
+// const welcomeMessage = isLoggedIn
+//   ? alert(
+//       "Welcome Guest! Just search Google for new proverbs and idioms in the language you are learning, learn and add them to the list."
+//     )
+//   : alert("Please log in")
