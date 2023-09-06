46c3e0053836208b4f30b71c47ddefe4fa4c5409
Minov Vitaliy <minov.vitaliy@tirolhelpsukraine.eu>
2023-09-06 20:07:07 +0200

    Morgenstund hat Gold im Mund.

---
+++ old-script.js
@@ -0,0 +1,172 @@
+function getProverb(sentence, callback) {
+  callback("The proverb of the day is: " + sentence)
+}
+
+getProverb("Wer dich vemisst, wird anrufen.", function (proverb) {
+  console.log(proverb)
+})
+
+getProverb("Wer nicht hören will, muss fühlen", (proverb) =>
+  console.log(proverb)
+)
+
+let proverbOfTheDay = (a, b) => {
+  return a + b
+}
+
+console.log(
+  proverbOfTheDay(
+    "The proverb of the day is: ",
+    "Wer nicht hören will, muss fühlen."
+  )
+)
+
+let printName = (name) => {
+  console.log(name)
+}
+
+printName("Guest")
+
+let printHi = (name) => "Hi " + name + "!"
+
+console.log(printHi("Guest"))
+
+let proverbArray = [
+  {
+    id: 1,
+    sentence: "Es ist noch kein Meister vom Himmel gefallen.",
+    points: 1,
+  },
+  {
+    id: 2,
+    sentence:
+      "Wer dich vermisst, wird anrufen. Wer dich will, sagt es. Wer Interesse hat, zeigt es.",
+    points: 1,
+  },
+  {
+    id: 3,
+    sentence:
+      "Die Lüge hat zwar kurze Beine, rennt aber schneller als die Wahrheit.",
+    points: 1,
+  },
+  {
+    id: 4,
+    sentence: "Für Geld und gute Worte kann man alles haben.",
+    points: 1,
+  },
+  {
+    id: 5,
+    sentence: "Wer nicht hören will, muss fühlen.",
+    points: 1,
+  },
+  {
+    id: 6,
+    sentence: "Reibung erzeugt Wärme.",
+    points: 1,
+  },
+  {
+    id: 7,
+    sentence: "Wer zuerst kommt, mahlt zuerst.",
+    points: 1,
+  },
+  {
+    id: 8,
+    sentence: "Wer nicht wagt, der nicht gewinnt.",
+    points: 1,
+  },
+  {
+    id: 9,
+    sentence: "Wer anderen eine Grube gräbt, fällt selbst hinein.",
+    points: 1,
+  },
+  {
+    id: 10,
+    sentence: "Morgenstund hat Gold im Mund.",
+    points: 1,
+  },
+]
+
+let proverbCount = proverbArray.reduce((sum, item) => {
+  return sum + item.points
+}, 0)
+
+console.log("Total proverbs count: " + proverbCount)
+
+let proverbFilter = proverbArray.filter((string) => {
+  return string.id > 0
+})
+
+console.log(proverbFilter)
+
+let proverbFind = proverbArray.find((string) => {
+  return string.id > 5
+})
+
+console.log(proverbFind)
+
+let proverbSome = proverbArray.some((string) => {
+  return (string.id = 3)
+})
+
+console.log(proverbSome)
+
+class Proverb {
+  constructor(sentence, language) {
+    this.sentence = sentence
+    this.language = language
+  }
+
+  printProverb() {
+    console.log(this.sentence)
+  }
+}
+
+let proverb = new Proverb(
+  "Wer zu spät kommt, den bestraft das Leben.",
+  "German"
+)
+
+let proverb7 = new Proverb("Wer nicht wagt, der nicht gewinnt.", "German")
+
+proverb7.printProverb()
+
+console.log(document.documentElement)
+
+const form = document.querySelector("#proverb-form")
+const list = document.querySelector("#newest-proverb")
+const input = document.querySelector("#proverb-input")
+const link = document.querySelector("[data-url]")
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  //   console.log(input.value)
+  const item = document.createElement("div")
+  item.innerText = input.value
+  item.classList.add("proverb")
+
+  list.appendChild(item)
+
+  input.value = ""
+})
+
+const h1 = document.getElementById("h1")
+
+h1.style.color = "black"
+
+const divProverb = document.getElementsByClassName("proverb")
+
+const divProverbWithArray = Array.from(divProverb)
+
+divProverbWithArray.forEach((div) => {
+  div.style.color = "grey"
+})
+
+const grandParent = document.querySelector("#proverb-block")
+const parentOne = grandParent.children[1]
+const parentTwo = parentOne.nextElementSibling
+const childOne = parentTwo.children[1]
+
+grandParent.style.color = "black"
+parentOne.style.color = "red"
+parentTwo.style.color = "blue"
+// childOne.style.color = "green"
--- script.js
+++ script.js
@@ -1,148 +1,26 @@
-function getProverb(sentence, callback) {
-  callback("The proverb of the day is: " + sentence)
-}
+// 1. Select all elements
+const form = document.querySelector("#proverb-form")
+const list = document.querySelector("#newest-proverb")
+const input = document.querySelector("#proverb-input")
 
-getProverb("Wer dich vemisst, wird anrufen.", function (proverb) {
-  console.log(proverb)
-})
-
-getProverb("Wer nicht hören will, muss fühlen", (proverb) =>
-  console.log(proverb)
-)
-
-let proverbOfTheDay = (a, b) => {
-  return a + b
-}
-
-console.log(
-  proverbOfTheDay(
-    "The proverb of the day is: ",
-    "Wer nicht hören will, muss fühlen."
-  )
-)
-
-let printName = (name) => {
-  console.log(name)
-}
-
-printName("Guest")
-
-let printHi = (name) => "Hi " + name + "!"
-
-console.log(printHi("Guest"))
-
-let proverbArray = [
-  {
-    id: 1,
-    sentence: "Es ist noch kein Meister vom Himmel gefallen.",
-    points: 1,
-  },
-  {
-    id: 2,
-    sentence:
-      "Wer dich vermisst, wird anrufen. Wer dich will, sagt es. Wer Interesse hat, zeigt es.",
-    points: 1,
-  },
-  {
-    id: 3,
-    sentence:
-      "Die Lüge hat zwar kurze Beine, rennt aber schneller als die Wahrheit.",
-    points: 1,
-  },
-  {
-    id: 4,
-    sentence: "Für Geld und gute Worte kann man alles haben.",
-    points: 1,
-  },
-  {
-    id: 5,
-    sentence: "Wer nicht hören will, muss fühlen.",
-    points: 1,
-  },
-  {
-    id: 6,
-    sentence: "Reibung erzeugt Wärme.",
-    points: 1,
-  },
-  {
-    id: 7,
-    sentence: "Wer zuerst kommt, mahlt zuerst.",
-    points: 1,
-  },
-  {
-    id: 8,
-    sentence: "Wer nicht wagt, der nicht gewinnt.",
-    points: 1,
-  },
-  {
-    id: 9,
-    sentence: "Wer anderen eine Grube gräbt, fällt selbst hinein.",
-    points: 1,
-  },
-]
-
-let proverbCount = proverbArray.reduce((sum, item) => {
-  return sum + item.points
-}, 0)
-
-console.log("Total proverbs count: " + proverbCount)
-
-let proverbFilter = proverbArray.filter((string) => {
-  return string.id > 0
-})
-
-console.log(proverbFilter)
-
-let proverbFind = proverbArray.find((string) => {
-  return string.id > 5
-})
-
-console.log(proverbFind)
-
-let proverbSome = proverbArray.some((string) => {
-  return (string.id = 3)
-})
-
-console.log(proverbSome)
-
-class Proverb {
-  constructor(sentence, language) {
-    this.sentence = sentence
-    this.language = language
-  }
-
-  printProverb() {
-    console.log(this.sentence)
-  }
-}
-
-let proverb = new Proverb(
-  "Wer zu spät kommt, den bestraft das Leben.",
-  "German"
-)
-
-let proverb7 = new Proverb("Wer nicht wagt, der nicht gewinnt.", "German")
-
-proverb7.printProverb()
-
-console.log(document.documentElement)
-
-const h1 = document.getElementById("h1")
-
-h1.style.color = "black"
-
-const divProverb = document.getElementsByClassName("proverb")
-
-const divProverbWithArray = Array.from(divProverb)
-
-divProverbWithArray.forEach((div) => {
-  div.style.color = "grey"
-})
-
-const form = document.querySelector("[data-form]")
-const link = document.querySelector("[data-url]")
-
+// 2. When I submit the form add a new element
 form.addEventListener("submit", (event) => {
+  //2.1 Prevent the default behaviour of a form
   event.preventDefault()
-  console.log("submitted form")
+
+  // 1. Create a new item
+  const item = document.createElement("div")
+  item.innerText = input.value
+  item.classList.add("newest-proverb")
+
+  // 2. Add that item to the list
+  list.appendChild(item)
+
+  // 3. Clear input
+  input.value = ""
+
+  // 4. Setup event listener to delete item when clicked
+  item.addEventListener("click", () => {
+    item.remove()
+  })
 })
