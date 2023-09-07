3df1ae088d7e70f7db330a31659506937bc4470b
Minov Vitaliy <minov.vitaliy@tirolhelpsukraine.eu>
2023-09-07 19:20:33 +0200

    Eigenlob stinkt.

--- wlc.js
+++ wlc.js
@@ -5,3 +5,8 @@ const isLoggedIn = true
 //       "Welcome Guest! Just search Google for new proverbs and idioms in the language you are learning, learn and add them to the list."
 //     )
 //   : alert("Please log in")
+
+// Short circuit evaluation used to call the default value for the user name if it's not provided (user is not logged in)
+function printUserName(name) {
+  console.log(name || "User")
+}
